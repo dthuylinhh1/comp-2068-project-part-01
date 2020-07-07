b3b2bfcb0493376c5ff56d6a9159140bb90e40a7
dthuylinhh1 <54965669+dthuylinhh1@users.noreply.github.com>
2020-07-07 17:25:29 +0700

    minor change

--- app.js
+++ app.js
@@ -87,5 +87,5 @@ app.use('/', routes);
 /*
   Step 8: Start the server
 */
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
